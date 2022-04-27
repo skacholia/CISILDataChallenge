0536e61cf88c8dccb2689beffa20e18b196bf600
joeornstein <jornstein@gmail.com>
2022-04-27 17:26:18 -0400

    aggregate to Census tract

--- code/question1/01_clean-and-summarize.R
+++ code/question1/01_clean-and-summarize.R
@@ -11,40 +11,31 @@ library(lubridate)
 library(sf)
 library(ggthemes)
 
-# boardings data by trip and by stop
+## 1. Load Data ---------------------------
+
+# load boardings data by trip and by stop
 trips <- read_csv('data/raw/question1/apc_detailed_09-01-2020_10-31-2020.csv')
 stops <- read_csv('data/raw/question1/stop_activity_granular_2020-09-01_2020-10-31-001.csv')
 
-# stops shapefile
+# load stops shapefile
 shp <- st_read('data/raw/KCM_Stops_Data/kcm_stops.shp') %>%
   select(stop_id = STOP_ID)
 
-# census tract boundaries
+# load census tract boundaries
 kctracts <- tigris::tracts(state = 53, county = 033, cb = TRUE, year = 2020) %>%
   select(GEOID)
 
-# for each stop, get the GEOID of the census tract it falls within:
+# for each stop, get the GEOID of the census tract it falls within
 shp <- st_transform(shp, crs = st_crs(kctracts))
 shp <- st_join(shp, kctracts, join = st_within)
 
-# tract-level selected variables from ACS
+# load tract-level selected variables from ACS
 acs <- read_csv("data/raw/King_County_ACS_2019_tract.csv") %>%
   select(GEOID,
          median_age = B01002_001E,
          population = B01003_001E,
          white = B02001_002E,
          median_income = B06011_001E)
-
-
-
-ggplot() +
-  geom_sf(data = kctracts,
-          alpha = 0.3,
-          mapping = aes(fill = median_income)) +
-  geom_sf(data = shp,
-          alpha = 0.1) +
-  coord_sf() +
-  theme_map()
 
 
 # time-varying confounders: covid rates and weather
@@ -55,6 +46,26 @@ covid <- read_csv('data/clean/covid.csv') %>%
                         format = '%m/%d/%Y'))
 
 weather <- read_csv('data/clean/weather.csv')
+
+
+## 2. Plot maps and time series ---------------------
+
+ggplot() +
+  geom_sf(data = kctracts,
+          alpha = 0.3) +
+  geom_sf(data = shp,
+          alpha = 0.1) +
+  coord_sf() +
+  theme_map()
+
+ggplot() +
+  geom_sf(data = filter(kctracts, GEOID == '53033006900'),
+          alpha = 0.3) +
+  geom_sf(data = filter(shp, GEOID == '53033006900'),
+          alpha = 0.5) +
+  coord_sf() +
+  theme_map()
+
 
 boardings_ts <- trips %>%
   mutate(date = as.Date(OPERATION_DATE,
@@ -164,17 +175,34 @@ ggplot(data = weather,
   theme_minimal() +
   labs(x='Date', y='Daily Precipitation')
 
-# merge with cleaned stops and trips data
+
+## 3. Clean Datasets ------------------------------
+
+# keep trips with at least 17 data points
+trips_to_keep <- trips %>%
+  count(TRIP_ID) %>%
+  filter(n >= 17)
+
+# merge with trips with covid and weather data
 trips <- trips %>%
+  filter(TRIP_ID %in% trips_to_keep$TRIP_ID) %>%
   left_join(covid, by = 'date') %>%
-  left_join(weather, by = 'date')
+  left_join(weather, by = 'date') %>%
+  select(trip_id = TRIP_ID,
+         date,
+         day_of_week,
+         psngr_boardings = PSNGR_BOARDINGS,
+         covid_cases,
+         avg_temp, precipitation)
 
 
+# keep stop-level boarding data for these trips only
 stops <- stops %>%
   select(date = OPERATION_DATE,
          trip_id = TRIP_ID,
          stop_id = STOP_ID,
          psngr_boardings = PSNGR_BOARDINGS) %>%
+  filter(trip_id %in% trips_to_keep$TRIP_ID) %>%
   mutate(date = as.Date(date),
          day_of_week = wday(date, label = TRUE)) %>%
   filter(date >= as.Date('2020-09-19'), # remove rides before the service change
@@ -182,27 +210,44 @@ stops <- stops %>%
          date <= treatment_date + weeks(2),
          day_of_week %in% c('Mon', 'Tue', 'Wed', 'Thu', 'Fri'))
 
+# compute tract-level daily boardings and merge with ACS
+tracts <- shp %>%
+  as_tibble() %>%
+  select(stop_id, GEOID) %>%
+  right_join(stops, by = 'stop_id') %>%
+  group_by(GEOID, date, day_of_week) %>%
+  summarize(total_boardings = sum(psngr_boardings)) %>%
+  ungroup
 
-## Write cleaned data to csv --------------------
+# create normalized boardings measure (relative to September 21)
+baseline <- tracts %>%
+  filter(date == '2020-09-21') %>%
+  select(GEOID, boardings_sep21 = total_boardings)
 
-trips %>%
-  select(trip_id = TRIP_ID,
-         date,
-         day_of_week,
-         psngr_boardings = PSNGR_BOARDINGS,
-         covid_cases,
-         avg_temp, precipitation) %>%
-  write_csv('data/clean/trips.csv')
+tracts <- tracts %>%
+  left_join(baseline, by = 'GEOID') %>%
+  # drop tracts with fewer than ten boardings on September 21
+  filter(boardings_sep21 >= 10) %>%
+  mutate(normalized_boardings = total_boardings / boardings_sep21)
 
-# keep trips with at least 17 data points
-trips_to_keep <- d %>%
-  count(trip_id) %>%
-  filter(n >= 17)
+gid <- '53033030008'
 
-stops %>%
-  filter(trip_id %in% trips_to_keep$TRIP_ID) %>%
-  write_csv('data/raw/stops.csv')
+ggplot(data = filter(tracts, GEOID == gid),
+       mapping = aes(x=date, y=normalized_boardings)) +
+  geom_point() +
+  theme_minimal() +
+  geom_vline(xintercept = as.Date('2020-10-01'), linetype = 'dashed')
 
-trips_recomputed <- stops %>%
-  group_by(date, trip_id) %>%
-  summarize(psngr_boardings = sum(psngr_boardings))
+
+# merge with ACS and time-varying covariates
+tracts <- tracts %>%
+  mutate(GEOID = as.numeric(GEOID)) %>%
+  left_join(acs, by = 'GEOID') %>%
+  left_join(covid, by = 'date') %>%
+  left_join(weather, by = 'date')
+
+## 4. Write cleaned data to csv --------------------
+
+write_csv(trips, 'data/clean/trips.csv')
+
+write_csv(tracts, 'data/clean/tracts.csv')
