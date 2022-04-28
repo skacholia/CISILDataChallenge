7f59814f4da3b5f1978ec1b0f2e653e0f29ea3f0
joeornstein <jornstein@gmail.com>
2022-04-28 14:26:58 -0400

    estimate heterogeneous treatment effects

--- code/question1/02_model.R
+++ code/question1/02_model.R
@@ -38,18 +38,18 @@ tracts$treated <- as.numeric(tracts$date >= treatment_date)
 
 ## 2. Estimate average treatment effect (ATE) ------------------------------
 
-twfe <- feols(psngr_boardings ~ treated + covid_cases +
-                avg_temp + precipitation | trip_id + day_of_week,
-              data = trips)
+m1 <- feols(psngr_boardings ~ treated + covid_cases +
+              avg_temp + precipitation | trip_id + day_of_week,
+            data = trips)
 
-summary(twfe) # standard errors clustered at the trip level
+summary(m1) # standard errors clustered at the trip level
 
-twfe_poisson <- fepois(psngr_boardings ~ treated + covid_cases +
-                         avg_temp + precipitation | trip_id + day_of_week,
-                       data = trips)
+m2 <- fepois(psngr_boardings ~ treated + covid_cases +
+               avg_temp + precipitation | trip_id + day_of_week,
+             data = trips)
 
-summary(twfe_poisson)
-summary(marginaleffects(twfe_poisson))
+summary(m2)
+summary(marginaleffects(m2))
 
 # note for my own edification: the Average Marginal Effect is computed like this
 # twfe_poisson %>%
@@ -70,7 +70,91 @@ ggplot(data = trips) +
 
 mean(trips$psngr_boardings)
 
-## 3. Estimate heterogeneous treatment effects at the tract level ------------------
+## 3. Estimate heterogeneous treatment effects by demographics at first stop --------------------
+
+trips_with_acs <- trips %>%
+  filter(!is.na(median_income)) %>%
+  mutate(pct_white = white/population*100)
+
+
+
+# estimate average treatment effect at three quantiles of median income
+m3 <- list(
+  'Poorest Third' = feols(psngr_boardings ~ treated + covid_cases +
+                            avg_temp + precipitation | trip_id + day_of_week,
+                          data = filter(trips_with_acs,
+                                        median_income < quantile(trips_with_acs$median_income,
+                                                                 0.33333))),
+  'Middle Third' = feols(psngr_boardings ~ treated + covid_cases +
+                           avg_temp + precipitation | trip_id + day_of_week,
+                         data = filter(trips_with_acs,
+                                       median_income >= quantile(trips_with_acs$median_income,
+                                                                 0.33333),
+                                       median_income < quantile(trips_with_acs$median_income,
+                                                                0.66666))),
+  'Richest Third' = feols(psngr_boardings ~ treated + covid_cases +
+                            avg_temp + precipitation | trip_id + day_of_week,
+                          data = filter(trips_with_acs,
+                                        median_income >= quantile(trips_with_acs$median_income,
+                                                                  0.66666)))
+)
+
+modelsummary(m3)
+modelplot(m3, coef_map = c('treated'='ATE'))
+
+
+# estimate average treatment effect at three quantiles of pct white
+m4 <- list(
+  'Least White' = feols(psngr_boardings ~ treated + covid_cases +
+                          avg_temp + precipitation | trip_id + day_of_week,
+                        data = filter(trips_with_acs,
+                                      pct_white < quantile(trips_with_acs$pct_white,
+                                                           0.33333))),
+  'Kinda White' = feols(psngr_boardings ~ treated + covid_cases +
+                          avg_temp + precipitation | trip_id + day_of_week,
+                        data = filter(trips_with_acs,
+                                      pct_white >= quantile(trips_with_acs$pct_white,
+                                                            0.33333),
+                                      pct_white < quantile(trips_with_acs$pct_white,
+                                                           0.66666))),
+  'Very White' = feols(psngr_boardings ~ treated + covid_cases +
+                         avg_temp + precipitation | trip_id + day_of_week,
+                       data = filter(trips_with_acs,
+                                     pct_white >= quantile(trips_with_acs$pct_white,
+                                                           0.66666)))
+)
+
+modelsummary(m4)
+modelplot(m4, coef_map = c('treated'='ATE'))
+
+
+# estimate average treatment effect at three quantiles of median_age
+m5 <- list(
+  'Youngest' = feols(psngr_boardings ~ treated + covid_cases +
+                          avg_temp + precipitation | trip_id + day_of_week,
+                        data = filter(trips_with_acs,
+                                      median_age < quantile(trips_with_acs$median_age,
+                                                           0.33333))),
+  'Middlest' = feols(psngr_boardings ~ treated + covid_cases +
+                          avg_temp + precipitation | trip_id + day_of_week,
+                        data = filter(trips_with_acs,
+                                      median_age >= quantile(trips_with_acs$median_age,
+                                                            0.33333),
+                                      median_age < quantile(trips_with_acs$median_age,
+                                                           0.66666))),
+  'Oldest' = feols(psngr_boardings ~ treated + covid_cases +
+                         avg_temp + precipitation | trip_id + day_of_week,
+                       data = filter(trips_with_acs,
+                                     median_age >= quantile(trips_with_acs$median_age,
+                                                           0.66666)))
+)
+
+modelsummary(m5)
+modelplot(m5, coef_map = c('treated'='ATE'))
+
+## 4. Estimate heterogeneous treatment effects at the tract level ------------------
+
+## IN PROGRESS..............
 
 gid <- '53033028403'
 
